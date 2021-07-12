6b17cfda7a9dcf9dd6355598cdb04cc6eebbeef7
AlexanderKllanxhja <akllanxhja@gmail.com>
2021-07-12 11:46:03 -0600

    commit tree 1aa5191dac723fb5a819c50067778a323d84edea

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
-function saturdayFun(activity="roller-skate") {
-   return (`This Saturday, I want to ${activity}!`);
+// function saturdayFun(activity="roller-skate") {
+//    return (`This Saturday, I want to ${activity}!`);
+// }
+// saturdayFun();
+
+// function mondayWork(activity="go to the office") {
+//     return (`This Monday, I will ${activity}.`);
+// }
+// mondayWork();
+
+// function wrapAdjective(style="*") {
+//     return function(adjective="special") {
+//         return `You are ${style}${adjective}${style}!`;
+//     }
+// }
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
 }
 saturdayFun();
 
-function mondayWork(activity="go to the office") {
-    return (`This Monday, I will ${activity}.`);
-}
-mondayWork();
+// Below is a 'function declaration'
+// function mondayWork(activity = 'go to the office') {
+//     return `This Monday, I will ${activity}.`;
+// }
+// mondayWork();
 
-function wrapAdjective(style="*") {
-    return function(adjective="special") {
+// This is the same function but is a 'function expression'
+let mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(style='*') {
+    return function(adjective='special') {
         return `You are ${style}${adjective}${style}!`;
     }
 }
+
+
